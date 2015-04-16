f81533db5ba31eb7c5c82262640d4173771f6e2e
Joakim Uddholm <joakim@uddholm.com>
2015-04-16 22:08:31 +0200

    PoC works. Not very secure tho (xss and csrfable.)

--- burn/static/burn.js
+++ burn/static/burn.js
@@ -5,25 +5,36 @@ module.controller("CreateCtl", ['$scope', '$http', function($scope, $http) {
   $scope.step = 1;
 
   var feedback = function(status, reason) {
+    console.log(reason);
     $scope.status = status;
     $scope.reason = reason;
   };
 
   var step2 = function(storage_key, encryption_key) {
-    $scope.share_url = "http://localhost:5000/"+storage_key+"#"+encryption_key;
+    feedback("info", "Done!");
+    $scope.share_url = "http://localhost:5000/"+storage_key+"#"+encodeURIComponent(encryption_key);
     $scope.step = 2;
   };
 
   $scope.send = function(message) {
     console.log(message);
+    var password = $scope.user_password;
+    if(!password || password == "" || password.length == 0) {
+      feedback("info", "generating a password...");
+      password = sjcl.codec.hex.fromBits(sjcl.random.randomWords(8));
+    }
+
+    console.log(password);
 
     feedback("info", "encrypting...");
+    message = sjcl.encrypt(password, message);
+    message = btoa(JSON.stringify(message));
 
     feedback("info", "sending to server...");
 
     $http.post("/create", {message: message})
     .success(function(data) {
-      step2(data, "");
+      step2(data, password);
     })
     .error(function(data) {
       feedback("error", data);
@@ -31,6 +42,13 @@ module.controller("CreateCtl", ['$scope', '$http', function($scope, $http) {
   };
 }]);
 
-module.controller("OpenCtl", ['$scope', '$http', function($scope, $http) {
-
+module.controller("OpenCtl", ['$scope', function($scope) {
+  var password = decodeURIComponent(document.location.hash);
+  password = password.substr(1, password.length-1);
+  console.log(password);
+  var message = document.getElementById("themessage").value;
+  console.log(message);
+  var debased = atob(message);
+  message = JSON.parse(debased);
+  $scope.decrypted = sjcl.decrypt(password, message);
 }]);
