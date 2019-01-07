2aea94b49decb45bf066a926ffdc4c92b726b8ed
Joakim Uddholm <joakim@uddholm.com>
2019-01-07 13:37:00 +0100

    Bump version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name         = 'burn-after-reading',
-    version      = '1.2',
+    version      = '1.2.1',
     packages     = find_packages(),
     test_suite   = "burn.tests",
     install_requires = ['Flask>=1.0.2'],    
