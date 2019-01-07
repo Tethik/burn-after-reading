b3d89584dbb6e508cf338f75d18dba336c72dc0b
Joakim Uddholm <joakim@uddholm.com>
2019-01-07 13:35:35 +0100

    Add Pipfile. Bump flask version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version      = '1.2',
     packages     = find_packages(),
     test_suite   = "burn.tests",
-    install_requires = ['Flask>=0.11.1'],    
+    install_requires = ['Flask>=1.0.2'],    
     zip_safe = False,
     include_package_data = True,
 )
