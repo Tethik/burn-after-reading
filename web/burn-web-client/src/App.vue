35ac3595d8f3ab35fc7c835ca87e79b1d79d926e
Joakim Uddholm <tethik@blacknode.se>
2025-05-22 13:34:46 +0200

    more initial css copied

--- web/burn-web-client/src/App.vue
+++ web/burn-web-client/src/App.vue
@@ -1,85 +1,43 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
+
 <template>
-  <!-- <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <div class="container" id="main">
+    <header>
+      <a href="/">
+        <img src="./assets/flame.png" />
+      </a>
+    </header>
+    <div id="content">
+      <RouterView />      
+      <footer>
+        <a href="/about">about</a>&nbsp;
+        <a href="https://github.com/Tethik/burn-after-reading">github</a>
+      </footer>
     </div>
-  </header> -->
-
-  <RouterView />
+  </div>
 </template>
 
 <style scoped>
 header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
   text-align: center;
-  margin-top: 2rem;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+footer {
+  text-align: right;
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+#main {
+  margin-top: 35px;
 }
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+#content {
+  padding: 40px;
+  border: 1px solid #95a5a6;
+  background-color: #ececec;
+  box-shadow: 4px 4px #95a5a6;
+  margin-bottom: 50px;
 }
 </style>
